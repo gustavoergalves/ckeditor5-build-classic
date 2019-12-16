084cda1bf639bdeb9d04ca854176d4e77a39ef50
Gustavo Guedes <gustavoergalves@gmail.com>
2019-12-16 10:03:28 -0300

    Ajustes de funções e tamanho da fonte

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -6,7 +6,6 @@ import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapte
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
@@ -39,7 +38,6 @@ ClassicEditor.builtinPlugins = [
 	Autoformat,
 	Bold,
 	Italic,
-	BlockQuote,
 	CKFinder,
 	EasyImage,
 	Heading,
@@ -72,7 +70,6 @@ ClassicEditor.defaultConfig = {
 			'|',
 			'bold',
 			'italic',
-			'fontfamily',
 			'fontsize',
 			'fontcolor',
 			'fontbackgroundcolor',
@@ -80,7 +77,6 @@ ClassicEditor.defaultConfig = {
 			'alignment:right',
 			'alignment:center',
 			'alignment:justify',
-			'blockQuote',
 			'link',
 			'|',
 			'imageViaUrlEmbed',
